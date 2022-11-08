67a9b2111771e7c2618cd001adb860572ffd3e75
João Gonçalves <joaopedro.jp.4998@gmail.com>
2022-11-08 18:24:13 +0000

    Add picture in picture logic

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
+const videoElement = document.getElementById("video");
+const startButton = document.getElementById("start-button");
+const otherButton = document.getElementById("other-button");
 
+async function selectMediaStream() {
+  try {
+    const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+    videoElement.srcObject = mediaStream;
+    videoElement.onloadedmetadata = () => {
+      videoElement.play();
+    };
+    startShare();
+  } catch {}
+}
+
+startButton.addEventListener("click", startShare);
+otherButton.addEventListener("click", selectMediaStream);
+
+async function startShare() {
+  startButton.disable = true;
+  await videoElement.requestPictureInPicture();
+  startButton.disable = false;
+}
+
+selectMediaStream();
